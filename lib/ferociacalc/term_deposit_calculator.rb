ddc4e67e31075dc96f721ee85c72dc1d27f052bc
Steven McPhillips <steven.mcphillips@gmail.com>
2025-07-16 13:28:00 +1000

    wip: replace input hashes with InputDefinition

--- lib/ferociacalc/term_deposit_calculator.rb
+++ lib/ferociacalc/term_deposit_calculator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'ferociacalc/result'
+require 'ferociacalc/input_definition'
 
 # A functional term deposit calculator
 module Ferociacalc
@@ -66,47 +67,47 @@ module Ferociacalc
     end
 
     def self.initial_deposit_input
-      {
-        option_type: ->(val) { Float(val) },
-        description: 'Required Initial deposit amount in dollars ($1_000.00 - $1_500_000.00)',
-        validator: lambda do |val|
-          raise "must provide a valid initial deposit amount (was #{val})" unless (val <= 1_500_500) && (val >= 1_000)
-        end,
-        transformer: ->(val) { val }
-      }
+      description = 'Required Initial deposit amount in dollars ($1_000.00 - $1_500_000.00)'
+      to_type = ->(val) { Float(val) }
+      validator = lambda do |val|
+        raise "must provide a valid initial deposit amount (was #{val})" unless (val <= 1_500_500) && (val >= 1_000)
+      end
+      transformer = ->(val) { val }
+
+      Ferociacalc::InputDefinition.new(:initial_deposit, description, to_type, validator, transformer)
     end
 
     def self.interest_rate_input
-      {
-        option_type: ->(val) { Float(val) },
-        description: 'Required Interest rate % p.a (0-15; e.g. 3% is 3, not 0.03)',
-        validator: lambda do |val|
-          raise "must provide a valid interest rate number (was #{val})" unless (val >= 0) && (val <= 15)
-        end,
-        transformer: ->(val) { (val / 100).to_f }
-      }
+      description = 'Required Interest rate % p.a (0-15; e.g. 3% is 3, not 0.03)'
+      to_type = ->(val) { Float(val) }
+      validator = lambda do |val|
+        raise "must provide a valid interest rate number (was #{val})" unless (val >= 0) && (val <= 15)
+      end
+      transformer = ->(val) { (val / 100).to_f }
+
+      Ferociacalc::InputDefinition.new(:interest_rate, description, to_type, validator, transformer)
     end
 
     def self.deposit_term_input
-      {
-        option_type: ->(val) { Integer(val) },
-        description: 'Required Deposit term in months (3-60; e.g. 12)',
-        validator: lambda do |val|
-          raise "must provide a valid number of months (was #{val})" unless (val >= 3) && (val <= 60)
-        end,
-        transformer: ->(val) { (val / 12.0) }
-      }
+      to_type = ->(val) { Integer(val) }
+      description = 'Required Deposit term in months (3-60; e.g. 12)'
+      validator = lambda do |val|
+        raise "must provide a valid number of months (was #{val})" unless (val >= 3) && (val <= 60)
+      end
+      transformer = ->(val) { (val / 12.0) }
+
+      Ferociacalc::InputDefinition.new(:deposit_term, description, to_type, validator, transformer)
     end
 
     def self.interest_frequency_input
-      {
-        option_type: ->(val) { String(val) },
-        description: "Required Interest payment period (i.e. #{interest_periods.keys.join(', ')})",
-        validator: lambda do |val|
-          raise "must provide a valid interest period (was #{val})" unless interest_periods.keys.include?(val)
-        end,
-        transformer: ->(val) { interest_periods[val] }
-      }
+      to_type = ->(val) { String(val) }
+      description = "Required Interest payment period (i.e. #{interest_periods.keys.join(', ')})"
+      validator = lambda do |val|
+        raise "must provide a valid interest period (was #{val})" unless interest_periods.keys.include?(val)
+      end
+      transformer = ->(val) { interest_periods[val] }
+
+      Ferociacalc::InputDefinition.new(:interest_frequency, description, to_type, validator, transformer)
     end
   end
 end
