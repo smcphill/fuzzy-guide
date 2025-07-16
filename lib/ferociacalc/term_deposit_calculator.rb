07d61c9c1503b7d125591188b2766f8e1114f074
Steven McPhillips <steven.mcphillips@gmail.com>
2025-07-16 13:03:14 +1000

    internal extract-method refactor: start chipping away at anonymous hash of input defs

--- lib/ferociacalc/term_deposit_calculator.rb
+++ lib/ferociacalc/term_deposit_calculator.rb
@@ -19,42 +19,13 @@ module Ferociacalc
     end
 
     # TODO: way too much responsibility for anonymous hashes (domain objects lurking)
-    # (also tightly coupled to 'optparse': not appropriate responsibility for the Calculator)
-    def self.inputs # rubocop:disable Metrics/MethodLength
+    def self.inputs
       # defines the inputs this calculator requires. the toplevel keys here are used by `#call`
       {
-        initial_deposit: {
-          option_type: ->(val) { Float(val) },
-          description: 'Required Initial deposit amount in dollars ($1_000.00 - $1_500_000.00)',
-          validator: lambda do |val|
-            raise "must provide a valid initial deposit amount (was #{val})" unless (val <= 1_500_500) && (val >= 1_000)
-          end,
-          transformer: ->(val) { val }
-        },
-        interest_rate: {
-          option_type: ->(val) { Float(val) },
-          description: 'Required Interest rate % p.a (0-15; e.g. 3% is 3, not 0.03)',
-          validator: lambda do |val|
-            raise "must provide a valid interest rate number (was #{val})" unless (val >= 0) && (val <= 15)
-          end,
-          transformer: ->(val) { (val / 100).to_f }
-        },
-        deposit_term: {
-          option_type: ->(val) { Integer(val) },
-          description: 'Required Deposit term in months (3-60; e.g. 12)',
-          validator: lambda do |val|
-            raise "must provide a valid number of months (was #{val})" unless (val >= 3) && (val <= 60)
-          end,
-          transformer: ->(val) { (val / 12.0) }
-        },
-        interest_frequency: {
-          option_type: ->(val) { String(val) },
-          description: "Required Interest payment period (i.e. #{interest_periods.keys.join(', ')})",
-          validator: lambda do |val|
-            raise "must provide a valid interest period (was #{val})" unless interest_periods.keys.include?(val)
-          end,
-          transformer: ->(val) { interest_periods[val] }
-        }
+        initial_deposit: initial_deposit_input,
+        interest_rate: interest_rate_input,
+        deposit_term: deposit_term_input,
+        interest_frequency: interest_frequency_input
       }
     end
 
@@ -83,6 +54,50 @@ module Ferociacalc
         'quarterly' => 4,
         'annually' => 1,
         'maturity' => 0
+      }
+    end
+
+    def self.initial_deposit_input
+      {
+        option_type: ->(val) { Float(val) },
+        description: 'Required Initial deposit amount in dollars ($1_000.00 - $1_500_000.00)',
+        validator: lambda do |val|
+          raise "must provide a valid initial deposit amount (was #{val})" unless (val <= 1_500_500) && (val >= 1_000)
+        end,
+        transformer: ->(val) { val }
+      }
+    end
+
+    def self.interest_rate_input
+      {
+        option_type: ->(val) { Float(val) },
+        description: 'Required Interest rate % p.a (0-15; e.g. 3% is 3, not 0.03)',
+        validator: lambda do |val|
+          raise "must provide a valid interest rate number (was #{val})" unless (val >= 0) && (val <= 15)
+        end,
+        transformer: ->(val) { (val / 100).to_f }
+      }
+    end
+
+    def self.deposit_term_input
+      {
+        option_type: ->(val) { Integer(val) },
+        description: 'Required Deposit term in months (3-60; e.g. 12)',
+        validator: lambda do |val|
+          raise "must provide a valid number of months (was #{val})" unless (val >= 3) && (val <= 60)
+        end,
+        transformer: ->(val) { (val / 12.0) }
+      }
+    end
+
+    def self.interest_frequency_input
+      {
+        option_type: ->(val) { String(val) },
+        description: "Required Interest payment period (i.e. #{interest_periods.keys.join(', ')})",
+        validator: lambda do |val|
+          raise "must provide a valid interest period (was #{val})" unless interest_periods.keys.include?(val)
+        end,
+        transformer: ->(val) { interest_periods[val] }
       }
     end
   end
