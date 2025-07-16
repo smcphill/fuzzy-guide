88e126baed82ad200ffc0deab87863c9018f842c
Steven McPhillips <steven.mcphillips@gmail.com>
2025-07-16 12:38:15 +1000

    remove optparse (needs test coverage, but also ready for extracting a CLI parse from CLI)

--- lib/ferociacalc/cli.rb
+++ lib/ferociacalc/cli.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'optparse'
 require 'ferociacalc/term_deposit_calculator'
 
 module Ferociacalc
@@ -37,33 +36,52 @@ module Ferociacalc
       puts formatted_result
     end
 
+    def self.print_help(expected_args, input_definitions)
+      input_details = expected_args.map {|arg| "#{arg}:    \t#{input_definitions[arg][:description]}"}
+
+      puts <<~HEREDOC
+        #{banner(expected_args)}
+
+        #{input_details.join("\n")}
+
+      HEREDOC
+    end
+
+    def self.banner(expected_args)
+      "Usage: ferociacalc <#{expected_args.join('> <')}>"
+    end
+
     # Inline parser: populates @defined_inputs as required by @calculator
     def parse_options(args)
-      parser = OptionParser.new do |p|
-        p.banner = 'Usage: ferociacalc [options]'
-        options_for_calculator_inputs(p)
+      calculator_args = %i[initial_deposit interest_rate deposit_term interest_frequency]
+      calculator_inputs = @calculator.class.inputs
+      if args.empty? || args.size != calculator_args.size
+        self.class.print_help(calculator_args, calculator_inputs)
+        raise 'Exiting'
       end
-      parser.parse!(args, into: @defined_inputs)
-      required_options = @calculator.class.inputs.keys
-      missing_options = required_options - @defined_inputs.keys
+
+      calculator_args.each_with_index do |arg, index|
+        transformed_value = self.class.extract_input(calculator_inputs[arg], args[index])
+
+        @defined_inputs[arg] = transformed_value
+      end
+
+      missing_options = calculator_args - @defined_inputs.keys
 
       return if missing_options.empty?
 
       # otherwise, print help and raise
-      puts parser
+      self.class.print_help(calculator_args, calculator_inputs)
       raise "Missing required options: #{missing_options.map(&:to_s).join(', ')}"
     end
 
-    # Setup parser to source named inputs from the calculator and retrieve them from user input
-    def options_for_calculator_inputs(parser)
-      @calculator.class.inputs.each do |input, details|
-        long_option = "--#{input} #{details[:long_opt]}"
-        parser.on(details[:short_opt], long_option, details[:option_type], details[:description]) do |option|
-          # this enforces any requirements the calculator imposes on the given option (i.e. non-negative balances!)
-          details[:requires].call(option)
-          @defined_inputs[input] = details[:marshal].call(option)
-        end
-      end
+    # applies the input definition logic against the provide value
+    def self.extract_input(input_definition, value)
+      typed_value = input_definition[:option_type].call(value)
+
+      input_definition[:validator].call(typed_value)
+
+      input_definition[:transformer].call(typed_value)
     end
   end
 end
