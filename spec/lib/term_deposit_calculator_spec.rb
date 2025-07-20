c8dea614649b5ac0595cc7c8f794f2d107bec2ab
Steven McPhillips <steven.mcphillips@gmail.com>
2025-07-21 08:33:13 +1000

    update test structure

--- spec/lib/term_deposit_calculator_spec.rb
+++ spec/lib/term_deposit_calculator_spec.rb
@@ -1,67 +1,69 @@
 require 'term_deposit_calculator'
 
 describe TermDepositCalculator do
-  let(:args) do
-    {
-      initial_deposit: 1000,
-      interest_rate: 0.035,
-      deposit_term: 1.0,
-      interest_frequency: 1
-    }
-  end
-
-  context 'with annual frequency' do
-    it 'works' do
-      expected_output = "    Final balance: $1035.00\n    Total interest earned:  $35.00\n"
-      expect(described_class.new.call(**args)).to eq(expected_output)
-    end
-  end
-
-  context 'with quarterly frequency' do
+  describe '#call' do
     let(:args) do
       {
         initial_deposit: 1000,
         interest_rate: 0.035,
         deposit_term: 1.0,
-        interest_frequency: 4
+        interest_frequency: 1
       }
     end
 
-    it 'works' do
-      expected_output = "    Final balance: $1035.00\n    Total interest earned:  $35.00\n"
-      expect(described_class.new.call(**args)).to eq(expected_output)
-    end
-  end
-
-  context 'with monthly frequency' do
-    let(:args) do
-      {
-        initial_deposit: 1000.0,
-        interest_rate: 0.035,
-        deposit_term: 1.0,
-        interest_frequency: 12
-      }
+    context 'with annual frequency' do
+      it 'works' do
+        expected_output = "    Final balance: $1035.00\n    Total interest earned:  $35.00\n"
+        expect(described_class.new.call(**args)).to eq(expected_output)
+      end
     end
 
-    it 'works' do
-      expected_output = "    Final balance: $1036.00\n    Total interest earned:  $36.00\n"
-      expect(described_class.new.call(**args)).to eq(expected_output)
-    end
-  end
+    context 'with quarterly frequency' do
+      let(:args) do
+        {
+          initial_deposit: 1000,
+          interest_rate: 0.035,
+          deposit_term: 1.0,
+          interest_frequency: 4
+        }
+      end
 
-  context 'with frequency at maturity' do
-    let(:args) do
-      {
-        initial_deposit: 1000.0,
-        interest_rate: 0.05,
-        deposit_term: 1.0,
-        interest_frequency: 0
-      }
+      it 'works' do
+        expected_output = "    Final balance: $1035.00\n    Total interest earned:  $35.00\n"
+        expect(described_class.new.call(**args)).to eq(expected_output)
+      end
     end
 
-    it 'works' do
-      expected_output = "    Final balance: $1050.00\n    Total interest earned:  $50.00\n"
-      expect(described_class.new.call(**args)).to eq(expected_output)
+    context 'with monthly frequency' do
+      let(:args) do
+        {
+          initial_deposit: 1000.0,
+          interest_rate: 0.035,
+          deposit_term: 1.0,
+          interest_frequency: 12
+        }
+      end
+
+      it 'works' do
+        expected_output = "    Final balance: $1036.00\n    Total interest earned:  $36.00\n"
+        expect(described_class.new.call(**args)).to eq(expected_output)
+      end
+    end
+
+    context 'with frequency at maturity' do
+      let(:args) do
+        {
+          initial_deposit: 1000.0,
+          interest_rate: 0.05,
+          deposit_term: 1.0,
+          interest_frequency: 0
+        }
+      end
+
+      it 'works' do
+        expected_output = "    Final balance: $1050.00\n    Total interest earned:  $50.00\n"
+        expect(described_class.new.call(**args)).to eq(expected_output)
+      end
     end
   end
 end
