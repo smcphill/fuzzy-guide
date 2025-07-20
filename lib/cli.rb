9236897d29a01c35713dc1bcd995f1c7bc415e0f
Steven McPhillips <steven.mcphillips@gmail.com>
2025-07-21 07:42:09 +1000

    parse interest frequencies

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,10 +5,43 @@ class CLI
   def call(args)
     # the calculator expects mixed time periods: term in months, rate and frequency per year
     TermDepositCalculator.new.call(
-      initial_deposit: args[0].to_f,
-      interest_rate: args[1].to_f,
-      deposit_term: args[2].to_i,
-      interest_frequency: args[3].to_i
+      initial_deposit: self.class.parse_initial_deposit(args[0]),
+      interest_rate: self.class.parse_interest_rate(args[1]),
+      deposit_term: self.class.parse_deposit_term(args[2]),
+      interest_frequency: self.class.parse_interest_frequency(args[3])
     )
+  end
+
+  # returns a float (dollars)
+  def self.parse_initial_deposit(initial_deposit)
+    initial_deposit.to_f
+  end
+
+  # returns a float (interest p.a.)
+  def self.parse_interest_rate(interest_rate)
+    (interest_rate.to_f / 100)
+  end
+
+  # returns a float (years)
+  def self.parse_deposit_term(deposit_term)
+    (deposit_term.to_f / 12)
+  end
+
+  # returns months per frequency period
+  # hardcoded to monthly for now
+  def self.parse_interest_frequency(interest_frequency)
+    frequency = frequency_periods[interest_frequency.to_sym]
+    raise 'Unknown interest frequency' if frequency.nil?
+
+    frequency.to_i
+  end
+
+  def self.frequency_periods
+    {
+      monthly: 12,
+      quarterly: 4,
+      annually: 1,
+      maturity: 0
+    }
   end
 end
--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -4,17 +4,17 @@ require 'rspec'
 require 'cli'
 
 describe CLI do
-  describe '#call' do
-    let(:args) { %w[1000 3.5 12 1]}
-    let(:expected_args) do
-      {
-        initial_deposit: 1000.0,
-        interest_rate: 3.5,
-        deposit_term: 12,
-        interest_frequency: 1
-      }
-    end
+  let(:args) { %w[1000 3.5 18 quarterly]}
+  let(:expected_args) do
+    {
+      initial_deposit: 1000.0,
+      interest_rate: 0.035,
+      deposit_term: 1.5,
+      interest_frequency: 4
+    }
+  end
 
+  describe '#call' do
     it 'calls TermDepositCalculator with args' do
       calculator = instance_double(TermDepositCalculator)
       allow(TermDepositCalculator).to receive(:new).and_return(calculator)
@@ -24,4 +24,45 @@ describe CLI do
       expect(calculator).to have_received(:call).with(expected_args)
     end
   end
+
+  describe '.parse_initial_deposit' do
+    it 'returns a float' do
+      expect(described_class.parse_initial_deposit(10)).to eq(10.0)
+    end
+  end
+
+  describe '.parse_interest_rate' do
+    it 'returns a decimalised float' do
+      expect(described_class.parse_interest_rate(10)).to eq(0.1)
+    end
+  end
+
+  describe '.parse_deposit_term' do
+    it 'returns a float of years' do
+      expect(described_class.parse_deposit_term(18)).to eq(1.5)
+    end
+  end
+
+  describe '.parse_interest_frequency' do
+    it 'returns 12 for monthly' do
+      expect(described_class.parse_interest_frequency('monthly')).to eq(12)
+    end
+
+    it 'returns 4 for quarterly' do
+      expect(described_class.parse_interest_frequency('quarterly')).to eq(4)
+    end
+
+    it 'returns 1 for annually' do
+      expect(described_class.parse_interest_frequency('annually')).to eq(1)
+    end
+
+    it 'returns 0 for maturity' do
+      expect(described_class.parse_interest_frequency('maturity')).to eq(0)
+    end
+
+    it 'raises on unexpected frequency' do
+      expect { described_class.parse_interest_frequency('unexpected') }.to raise_error.with_message('Unknown interest frequency')
+    end
+  end
+
 end
